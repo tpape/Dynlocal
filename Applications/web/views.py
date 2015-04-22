e92fd1623cdbe715862506792db81fd7fd3779b4
Hosni <hosni.dahmani@gmail.com>
2015-04-22 09:22:23 +0200

    import js and css libs, import a static file for testing

--- Applications/web/views.py
+++ Applications/web/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response, RequestContext
 
 def index(request):
-    return render(request, 'index.html',{})
+    return render_to_response('index.html', locals(), RequestContext(request))

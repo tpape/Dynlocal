143d271160a135f8f12587b100fda39c92e3180a
Hosni <hosni.dahmani@gmail.com>
2015-04-25 14:08:58 +0200

    working registration form with password confirmation check and email uniqueness check

--- Applications/web/forms.py
+++ Applications/web/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.core.exceptions import ValidationError
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext as _
 from django.contrib.auth.models import User
 from Applications.api.models import UserProfile
 
@@ -21,8 +21,15 @@ class UserForm(forms.ModelForm):
                 raise forms.ValidationError(_("La confirmation ne correspond pas au mot de passex"), code='invalid')
             return confirmation
 
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if email and User.objects.filter(email=email).count() > 0:
+            raise forms.ValidationError(_("Cette adresse existe déjà"), code='invalid')
+        return email
+
 class UserProfileForm(forms.ModelForm):
 
-       class Meta:
+    class Meta:
         model = UserProfile
-        exclude = ('registration_history',)
+        exclude = ('registration_history','user_id','user')
--- Applications/web/views.py
+++ Applications/web/views.py
@@ -12,17 +12,19 @@ def register(request):
         user_form = UserForm(data=request.POST)
         profile_form = UserProfileForm(data=request.POST)
         registered = True
+        print(profile_form)
         if user_form.is_valid() and profile_form.is_valid():
             user = user_form.save() #save te user's form data to the database
             user.set_password(user.password) # hash password
             user.save()
 
-            profile = profile_form.save(commit=false)
+            profile = profile_form.save(commit=False)
             profile.user = user
             profile.save()
             registered = True
         else:
-            print(user_form.errors)
+            print(user_form.errors, profile_form.errors)
     else:
         user_form = UserForm()
-    return render_to_response('contrib.html',{'user_form':user_form, 'registered':registered}, context)
+        profile_form = UserProfileForm()
+    return render_to_response('contrib.html',{'user_form':user_form, 'profile_form':profile_form ,'registered':registered}, context)

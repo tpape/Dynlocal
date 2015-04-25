d50ca99c73e362605368231a7909105187ae87d7
Hosni <hosni.dahmani@gmail.com>
2015-04-25 17:29:18 +0200

    added login form with working authentication and reidrection to next views

--- Applications/web/urls.py
+++ Applications/web/urls.py
@@ -4,6 +4,8 @@ from Applications.web import views
 urlpatterns = [
     # Template URLs
     url(r'^$', views.index),
-    url(r'contrib/$',views.register),
+    url(r'contrib/$', views.register),
+    url(r'contrib/register$',views.register),
+    url(r'contrib/login$', views.user_login),
 #    url(r'register/$', views.register),
 ]
--- Applications/web/views.py
+++ Applications/web/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, render_to_response
 from django.template import RequestContext
+from django.contrib.auth import authenticate, login
 from Applications.web.forms import UserForm, UserProfileForm
 
 def index(request):
@@ -14,7 +15,7 @@ def register(request):
         registered = True
         print(profile_form)
         if user_form.is_valid() and profile_form.is_valid():
-            user = user_form.save() #save te user's form data to the database
+            user = user_form.save() #save the user's form data to the database
             user.set_password(user.password) # hash password
             user.save()
 
@@ -28,3 +29,24 @@ def register(request):
         user_form = UserForm()
         profile_form = UserProfileForm()
     return render_to_response('contrib.html',{'user_form':user_form, 'profile_form':profile_form ,'registered':registered}, context)
+def user_login(request):
+    context=RequestContext(request)
+    loged_in = False
+
+    if request.method== 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                loged_in = True
+                return render_to_response('contrib.html',{'loged_in':loged_in}, context)
+            else:
+                return HttpResponse('Votre compte est désactivé.')
+        else:
+            return HttpResponse('Vos identifiants sont invalides.')
+    else:
+        return render_to_response('contrib.html',{'loged_in':loged_in}, context)

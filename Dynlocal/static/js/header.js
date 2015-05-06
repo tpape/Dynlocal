d9303c1b97165190001edd2465e3027ee128fece
Hosni <hosni.dahmani@gmail.com>
2015-05-06 23:08:44 +0200

    enhanced header a logo to improove

---
+++ Dynlocal/static/js/header.js
@@ -0,0 +1,20 @@
+function shrink_scroll() {
+    window.addEventListener('scroll', function(e){
+        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+            shrinkOn = 300,
+            header = document.querySelector("header");
+        if (distanceY > shrinkOn) {
+            classie.add(header,"smaller");
+        } else {
+            if (classie.has(header,"smaller")) {
+                classie.remove(header,"smaller");
+            }
+        }
+    });
+}
+window.onload = shrink_scroll();
+
+function shrink_click(){
+  header = document.querySelector("header");
+  classie.add(header,"smaller");
+}
